cd734e76c27677be0f9c773b427fe867b58918f4
Ohai Chukwuemeka <chukwuemeka.ohai@decagon.dev>
2024-12-17 23:26:43 +0100

    updated register endpoint

--- src/main/java/com/ifarmr/service/UserService.java
+++ src/main/java/com/ifarmr/service/UserService.java
@@ -1,11 +1,10 @@
 package com.ifarmr.service;
 
-import com.ifarmr.entity.enums.Gender;
 import com.ifarmr.entity.User;
+import com.ifarmr.entity.enums.Gender;
 import com.ifarmr.payload.request.LoginRequestDto;
-import com.ifarmr.payload.request.UpdateUserRequestDto;
-import com.ifarmr.payload.response.AuthResponse;
 import com.ifarmr.payload.request.RegistrationRequest;
+import com.ifarmr.payload.request.UpdateUserRequestDto;
 import com.ifarmr.payload.response.AuthResponse;
 import com.ifarmr.payload.response.LoginResponse;
 
