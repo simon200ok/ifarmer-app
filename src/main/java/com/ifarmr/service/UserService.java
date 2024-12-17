677867e11a5af765fd8ce44884cc177535cc5db4
Ndamziakani <kingsleyakani5@gmail.com>
2024-12-17 21:17:44 +0100

    user update implementation

--- src/main/java/com/ifarmr/service/UserService.java
+++ src/main/java/com/ifarmr/service/UserService.java
@@ -1,6 +1,8 @@
 package com.ifarmr.service;
 
+import com.ifarmr.entity.User;
 import com.ifarmr.payload.request.LoginRequestDto;
+import com.ifarmr.payload.request.UpdateUserRequestDto;
 import com.ifarmr.payload.response.AuthResponse;
 import com.ifarmr.payload.request.RegistrationRequest;
 import com.ifarmr.payload.response.LoginResponse;
@@ -10,5 +12,8 @@ public interface UserService {
     AuthResponse register(RegistrationRequest request);
 
     LoginResponse login(LoginRequestDto request);
+
+    User updateUser(Long userId, UpdateUserRequestDto request);
+
 }
 
