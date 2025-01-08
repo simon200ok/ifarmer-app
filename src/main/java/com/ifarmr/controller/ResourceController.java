97473a54cc6f4ce3f68bb7473c988471510ef0ef
Ohai Chukwuemeka <chukwuemeka.ohai@decagon.dev>
2025-01-08 12:30:22 +0100

    resolved conflicts

--- src/main/java/com/ifarmr/controller/ResourceController.java
+++ src/main/java/com/ifarmr/controller/ResourceController.java
@@ -4,16 +4,12 @@ package com.ifarmr.controller;
 import com.ifarmr.entity.User;
 import com.ifarmr.payload.response.TotalResourcesDTO;
 import com.ifarmr.service.ResourceService;
-import com.ifarmr.payload.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user/resources")
