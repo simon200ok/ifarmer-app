ceb88f8d9e67ea24056bb9a6e8fda78374b9cd03
Klenzdo <fagwaigbo@gmail.com>
2025-01-09 01:50:23 +0100

    Updated AnimalDetailsRepository

--- src/main/java/com/ifarmr/repository/AnimalDetailsRepository.java
+++ src/main/java/com/ifarmr/repository/AnimalDetailsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnimalDetailsRepository extends JpaRepository<AnimalDetails, Long> {
+    int countByUserId(Long userId);
 }
